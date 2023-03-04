0b24f382380b5aef24cda08207c0613140d6e9a6
Love Engman <love.engman@vidispine.com>
2023-03-04 11:10:41 +0200

    Fix build warnings

--- Stix/Models/QueryOptions.cs
+++ Stix/Models/QueryOptions.cs
@@ -4,21 +4,21 @@ namespace Stix.Models;
 // WIP
 public class QueryOptions
 {
-    public IList<FilterBy> Filter { get; set; } = new List<FilterBy>();
-    public SortBy SortBy { get; set; }
+    public IList<FilterBy>? Filter { get; set; } = new List<FilterBy>();
+    public SortBy? SortBy { get; set; }
     public int Offset { get; set; }
     public int Limit { get; set; }
 }
 
 public class FilterBy
 {
-    public string Property { get; set; }
-    public object Value { get; set; }
+    public string? Property { get; set; }
+    public object? Value { get; set; }
 }
 
 public class SortBy
 {
-    public string Property{ get; set; }
+    public string? Property{ get; set; }
     public SortOrder Order { get; set; }
 }
 
